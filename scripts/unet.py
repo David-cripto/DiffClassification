375efe3fbd46485f52c2cdb0269b5f53647964d3
Viktoriia Zinkovich <78615928+victoriazinkovich@users.noreply.github.com>
2024-02-22 12:18:21 +0300

    Add files via upload

---
+++ scripts/unet.py
@@ -0,0 +1,217 @@
+import math
+
+import torch
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset, DataLoader
+
+
+class LabeledDataset(Dataset):
+    def __init__(self, X, y, drop_label=0.0):
+        assert len(X) == len(y)
+        self.X = X.astype("float32")
+        self.y = y.astype("int")
+        self.drop_label = drop_label
+        self.null_label = int(max(y)) + 1
+
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        x, y = self.X[idx], self.y[idx]
+        if torch.rand(1) < self.drop_label:
+            y = self.null_label
+        return {"x": x, "y": y}
+    
+def get_labeled_data_loader(X, y, batch_size=1, shuffle=False, drop_label=0.0):
+    return DataLoader(
+        LabeledDataset(X, y, drop_label),
+        batch_size=batch_size,
+        shuffle=shuffle,
+    )
+
+
+class MyBlock(nn.Module):
+    def __init__(self, shape, in_c, out_c, kernel_size=3, stride=1, padding=1, activation=None, normalize=True):
+        super(MyBlock, self).__init__()
+        self.ln = nn.LayerNorm(shape)
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size, stride, padding)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size, stride, padding)
+        self.activation = nn.SiLU() if activation is None else activation
+        self.normalize = normalize
+
+    def forward(self, x):
+        out = self.ln(x) if self.normalize else x
+        out = self.conv1(out)
+        out = self.activation(out)
+        out = self.conv2(out)
+        out = self.activation(out)
+        return out
+
+
+class MyUNet(nn.Module):
+    def __init__(self, n_steps=1000, time_emb_dim=128, use_null_cond=False):
+        super().__init__()
+
+        # Sinusoidal embedding
+        self.time_embed = nn.Embedding(n_steps, time_emb_dim)
+        self.time_embed.weight.data = sinusoidal_embedding(n_steps, time_emb_dim)
+        self.time_embed.requires_grad_(False)
+        self.y_embed = nn.Embedding(10 + int(use_null_cond), time_emb_dim)
+        self.is_y_cond = True
+
+        # First half
+        self.te1 = self._make_te(time_emb_dim, 1)
+        self.b1 = nn.Sequential(
+            MyBlock((1, 28, 28), 1, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10)
+        )
+        self.down1 = nn.Conv2d(10, 10, 4, 2, 1)
+
+        self.te2 = self._make_te(time_emb_dim, 10)
+        self.b2 = nn.Sequential(
+            MyBlock((10, 14, 14), 10, 20),
+            MyBlock((20, 14, 14), 20, 20),
+            MyBlock((20, 14, 14), 20, 20)
+        )
+        self.down2 = nn.Conv2d(20, 20, 4, 2, 1)
+
+        self.te3 = self._make_te(time_emb_dim, 20)
+        self.b3 = nn.Sequential(
+            MyBlock((20, 7, 7), 20, 40),
+            MyBlock((40, 7, 7), 40, 40),
+            MyBlock((40, 7, 7), 40, 40)
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(40, 40, 2, 1),
+            nn.SiLU(),
+            nn.Conv2d(40, 40, 4, 2, 1)
+        )
+
+        # Bottleneck
+        self.te_mid = self._make_te(time_emb_dim, 40)
+        self.b_mid = nn.Sequential(
+            MyBlock((40, 3, 3), 40, 20),
+            MyBlock((20, 3, 3), 20, 20),
+            MyBlock((20, 3, 3), 20, 40)
+        )
+
+        # Second half
+        self.up1 = nn.Sequential(
+            nn.ConvTranspose2d(40, 40, 4, 2, 1),
+            nn.SiLU(),
+            nn.ConvTranspose2d(40, 40, 2, 1)
+        )
+
+        self.te4 = self._make_te(time_emb_dim, 80)
+        self.b4 = nn.Sequential(
+            MyBlock((80, 7, 7), 80, 40),
+            MyBlock((40, 7, 7), 40, 20),
+            MyBlock((20, 7, 7), 20, 20)
+        )
+
+        self.up2 = nn.ConvTranspose2d(20, 20, 4, 2, 1)
+        self.te5 = self._make_te(time_emb_dim, 40)
+        self.b5 = nn.Sequential(
+            MyBlock((40, 14, 14), 40, 20),
+            MyBlock((20, 14, 14), 20, 10),
+            MyBlock((10, 14, 14), 10, 10)
+        )
+
+        self.up3 = nn.ConvTranspose2d(10, 10, 4, 2, 1)
+        self.te_out = self._make_te(time_emb_dim, 20)
+        self.b_out = nn.Sequential(
+            MyBlock((20, 28, 28), 20, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10, normalize=False)
+        )
+
+        self.conv_out = nn.Conv2d(10, 1, 3, 1, 1)
+
+
+    def forward(self, x, t, y):
+        # x is (N, 2, 28, 28) (image with positional embedding stacked on channel dimension)  
+        t = self.time_embed(t)
+        if self.is_y_cond:
+            t += self.y_embed(y)
+        n = len(x)
+        out1 = self.b1(x + self.te1(t).reshape(n, -1, 1, 1))  # (N, 10, 28, 28)
+        out2 = self.b2(self.down1(out1) + self.te2(t).reshape(n, -1, 1, 1))  # (N, 20, 14, 14)
+        out3 = self.b3(self.down2(out2) + self.te3(t).reshape(n, -1, 1, 1))  # (N, 40, 7, 7)
+
+        out_mid = self.b_mid(self.down3(out3) + self.te_mid(t).reshape(n, -1, 1, 1))  # (N, 40, 3, 3)
+
+        out4 = th.cat((out3, self.up1(out_mid)), dim=1)  # (N, 80, 7, 7)
+        out4 = self.b4(out4 + self.te4(t).reshape(n, -1, 1, 1))  # (N, 20, 7, 7)
+
+        out5 = th.cat((out2, self.up2(out4)), dim=1)  # (N, 40, 14, 14)
+        out5 = self.b5(out5 + self.te5(t).reshape(n, -1, 1, 1))  # (N, 10, 14, 14)
+
+        out = th.cat((out1, self.up3(out5)), dim=1)  # (N, 20, 28, 28)
+        out = self.b_out(out + self.te_out(t).reshape(n, -1, 1, 1))  # (N, 1, 28, 28)
+
+        out = self.conv_out(out)
+
+        return out
+
+
+    def _make_te(self, dim_in, dim_out):
+        return nn.Sequential(
+            nn.Linear(dim_in, dim_out),
+            nn.SiLU(),
+            nn.Linear(dim_out, dim_out)
+        )
+
+
+    def get_features(self, x, t, y):
+        t = self.time_embed(t)
+        if self.is_y_cond:
+            t += self.y_embed(y)
+
+        n = len(x)
+
+        out1 = self.b1(x + self.te1(t).reshape(n, -1, 1, 1))  # (N, 10, 28, 28)
+        out2 = self.b2(self.down1(out1) + self.te2(t).reshape(n, -1, 1, 1))  # (N, 20, 14, 14)
+        out3 = self.b3(self.down2(out2) + self.te3(t).reshape(n, -1, 1, 1))  # (N, 40, 7, 7)
+
+        out_mid = self.b_mid(self.down3(out3) + self.te_mid(t).reshape(n, -1, 1, 1))  # (N, 40, 3, 3)
+
+        out4 = th.cat((out3, self.up1(out_mid)), dim=1)  # (N, 80, 7, 7)
+        out4 = self.b4(out4 + self.te4(t).reshape(n, -1, 1, 1))  # (N, 20, 7, 7)
+
+        out5 = th.cat((out2, self.up2(out4)), dim=1)  # (N, 40, 14, 14)
+        out5 = self.b5(out5 + self.te5(t).reshape(n, -1, 1, 1))  # (N, 10, 14, 14)
+
+        out = th.cat((out1, self.up3(out5)), dim=1)  # (N, 20, 28, 28)
+        out = self.b_out(out + self.te_out(t).reshape(n, -1, 1, 1))  # (N, 1, 28, 28)
+
+        #out = self.conv_out(out)
+
+        out_mid_avg = F.avg_pool2d(out_mid, kernel_size=out_mid.shape[2:])
+        out_mid_avg = th.reshape(out_mid_avg, (n, -1))
+
+        out4_avg = F.avg_pool2d(out4, kernel_size=out4.shape[2:])
+        out4_avg = th.reshape(out4_avg, (n, -1))
+
+        out5_avg = F.avg_pool2d(out5, kernel_size=out5.shape[2:])
+        out5_avg = th.reshape(out5_avg, (n, -1))
+
+        out = F.avg_pool2d(out, kernel_size=out.shape[2:])
+        out = th.reshape(out, (n, -1))
+
+        feature_map = th.cat((out_mid_avg, out4_avg, out5_avg, out), dim=1)
+
+        return feature_map
+
+
+def sinusoidal_embedding(n, d):
+    embedding = th.tensor([[i / 10_000 ** (2 * j / d) for j in range(d)] for i in range(n)])
+    sin_mask = th.arange(0, n, 2)
+
+    embedding[sin_mask] = th.sin(embedding[sin_mask])
+    embedding[1 - sin_mask] = th.cos(embedding[sin_mask])
+
+    return embedding
