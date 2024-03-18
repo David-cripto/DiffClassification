bbd1772edbf021ae53acbad8700e3bbfaa33d9d7
David-cripto <li.d@phystech.edu>
2024-03-18 15:44:39 +0300

    scripts

---
+++ scripts/mnist_train.py
@@ -0,0 +1,43 @@
+import torch as th
+import random
+from diffclassification.diffusion import DDPM
+from diffclassification.unet import MyUNet
+from diffclassification.mnistdataset import get_train_data_loader
+from diffclassification.utils import get_beta_schedule, train_model
+import numpy as np
+import argparse
+
+
+def main(args):
+    T = 1000
+    BATCH_SIZE = 128
+    LR = 0.001
+    WEIGHT_DECAY = 0.0
+    N_EPOCH = 30
+
+    th.manual_seed(0)
+    random.seed(0)
+
+    model = MyUNet().to("cuda")
+
+    ddpm = DDPM(betas=get_beta_schedule(T), model=model)
+    dataloader = get_train_data_loader(args.path_to_mnist, batch_size=BATCH_SIZE, shuffle=True)
+
+    train_model(
+        ddpm=ddpm,
+        dataloader=dataloader,
+        lr=LR,
+        weight_decay=WEIGHT_DECAY,
+        n_epoch=N_EPOCH,
+        device="cuda"
+    )
+    th.save(ddpm.to("cpu").state_dict(), args.path_to_save)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                    prog='MNIST train',
+                    description='train diffusion on mnist dataset and save wights in path')
+    parser.add_argument("path_to_mnist", type=str)
+    parser.add_argument("path_to_save", type=str)
+    args = parser.parse_args()
+    main(args)
